84cff126deb49056997038b697c044e28f4a6bac
Alexandre-vale <v-alexan@hotmail.com>
2021-07-14 17:16:24 -0300

    test2

--- src/main/java/com/alexandre/tryn5/controller/LoginController.java
+++ src/main/java/com/alexandre/tryn5/controller/LoginController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PutMapping;
 
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class LoginController {
     @Autowired
--- src/main/java/com/alexandre/tryn5/controller/PacienteController.java
+++ src/main/java/com/alexandre/tryn5/controller/PacienteController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class PacienteController {
 
