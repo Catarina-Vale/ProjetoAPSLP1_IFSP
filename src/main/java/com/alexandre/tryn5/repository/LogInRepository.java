d6770f6620e4f90d2a158390a801beadcd568e4f
Alexandre-vale <v-alexan@hotmail.com>
2021-07-14 17:27:50 -0300

    clean

--- src/main/java/com/alexandre/tryn5/repository/LogInRepository.java
+++
@@ -1,9 +0,0 @@
-package com.alexandre.tryn5.repository;
-
-import com.alexandre.tryn5.model.User;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface LogInRepository extends MongoRepository<User, String> {
-    
-}
--- src/main/java/com/alexandre/tryn5/repository/PacienteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.alexandre.tryn5.repository;
-
-import com.alexandre.tryn5.model.Paciente;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface PacienteRepository extends MongoRepository<Paciente, String> {
-    
-}
